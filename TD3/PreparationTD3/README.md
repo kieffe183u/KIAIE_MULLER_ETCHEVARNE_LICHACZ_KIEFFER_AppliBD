936f30672606a178e7667b4ed8842460bc25f997
kieffe183u <charlie.kieffer1@etu.univ-lorraine.fr>
2021-03-18 09:40:44 +0100

    avancé de la seance

--- TD3/PreparationTD3/README.md
+++ TD3/PreparationTD3/README.md
@@ -32,7 +32,7 @@ $time = $timeend - $timestart;
  
 //affichage du temps
 
-echo "la durée d'execution est de $time secondes \n";
+echo "la durée d'execution est de ".$time." secondes <br>";
 
 2. Lorsque nous faisons une recherche précise dans des tables avec un volume de données important, il faut parcourir l'entiereté des lignes de la table. L'index va permettre d'accélérer ce processus de recherche, notamment par le tri des données indexées. Par exemple pour rechercher une personne grâce à son nom et son prénom, nous allons procéder par dichotomie et donc gagner beaucoup de temps.
 
--- TD3/index.php
+++ TD3/index.php
@@ -22,3 +22,58 @@ $db->bootEloquent();
 
 //TD3
 
+// Partie 1
+
+//Question 1
+
+$tmp1 = microtime(true);
+$Question1 = Game::select('game.name')->get();
+$tmp2 = microtime(true);
+$tmpQ1 = $tmp2 - $tmp1;
+
+
+echo '<h1> Question 1 </h1>';
+echo "la durée d'execution est de ".$tmpQ1." secondes <br>";
+
+//Question 2
+
+$tmp3 = microtime(true);
+$Question2 =Game::select('game.name')->where('game.name','like', 'Mario%');
+$tmp4 = microtime(true);
+$tmpQ2 = $tmp4 - $tmp3;
+
+echo '<h1> Question 2 </h1>';
+echo "la durée d'execution est de ".$tmpQ2." secondes <br>";
+
+//Question 3
+
+$tmp5 = microtime(true);
+$Question3 = Game::where('name', 'LIKE', 'Mario%')->get();
+foreach ($Question3 as $l){
+    $perso = $l->characters;
+    foreach ($perso as $p) {
+    }
+}
+$tmp6 = microtime(true);
+$tmpQ3 = $tmp6 - $tmp5;
+
+echo '<h1> Question 3 </h1>';
+echo "la durée d'execution est de ".$tmpQ3." secondes <br>";
+
+//Question 4
+
+$tmp7 = microtime(true);
+$Question4 = Game::where('name', 'LIKE', 'Mario%')->get();
+    foreach ($Question4 as $l){
+
+        if(strpos($l->rating()->first(),"3+")) {
+        }
+    }
+$tmp8 = microtime(true);
+$tmpQ4 = $tmp8 - $tmp7;
+
+echo '<h1> Question 4 </h1>';
+echo "la durée d'execution est de ".$tmpQ4." secondes <br>";
+
+
+
