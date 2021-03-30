a6849730204c7e6a89d628901ce8a8c1a1c633e9
Léo Muller <leomllr8@gmail.com>
2021-03-30 15:45:25 +0200

    lourd

--- TD5-6/app/controller/Game.php
+++ TD5-6/app/controller/Game.php
@@ -1,25 +1,24 @@
 <?php
 
 namespace gamepedia\controller;
-
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Slim\Http\Request as Request;
+use Slim\Http\Response as Response;
 
 class Game
 {
-
-
     public function showGame(Request $rq, Response $rs, array $args): Response
     {
         $id = $args['id'];
         try {
-        $game = \gamepedia\modele\Game::select("")->where('id', '=', $id)->firstOrFail();
+            $game = \gamepedia\modele\Game::select("")->where('id', '=', $id)->firstOrFail();
 
-        $tmp = json_encode($game, JSON_FORCE_OBJECT);
-            $rs = $rs->write("<h1>".$tmp."</h1>");
+            $tmp = json_encode($game, JSON_FORCE_OBJECT);
+            $rs = $rs->getBody()->write("<h1>".$tmp."</h1>");
             $rs = $rs->withHeader("Content-Type", "application/json");
-        } catch (ModelNotFoundException){
+        } catch (Exception $e){
             $rs = $rs->withStatus(404,"Modele not found gros bg");
         }
+
         return $rs;
     }
 
--- TD5-6/index.php
+++ TD5-6/index.php
@@ -1,24 +1,17 @@
 <?php
 
 use Illuminate\Database\Capsule\Manager as DB;
-use gamepedia\modele\Game as Game;
-use gamepedia\modele\Company as Company;
-use gamepedia\modele\Platform as Platform;
-use gamepedia\modele\Game2character as Game2Character;
-use gamepedia\modele\Character as Character;
-use gamepedia\modele\Rating_board as Rating_board;
-use gamepedia\modele\User as User;
-use gamepedia\modele\Commentary as Commentary;
+use Slim\Container;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 //-------- Config --------//
-
-$app = new \Slim\App();
-
 $db = new DB();
 $creds = parse_ini_file("creds.ini");
-$container = new \Slim\Container($creds);
+$creds = ['settings' => [
+    'displayErrorDetails' => true
+]];
+$container = new Container($creds);
 $app = new \Slim\App($container);
 if ($creds) $db->addConnection($creds);
 $db->setAsGlobal();
