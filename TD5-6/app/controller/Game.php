a889833075a714328fb8629a810d2bd5a3748927
Léo Muller <leomllr8@gmail.com>
2021-03-30 17:00:35 +0200

    oue2

--- TD5-6/app/controller/Game.php
+++ TD5-6/app/controller/Game.php
@@ -10,12 +10,12 @@ class Game
     public function showGame(Request $rq, Response $rs, array $args): Response
     {
         $id = $args['id'];
+
         try {
             $game = \gamepedia\modele\Game::select("id","name","alias","deck","description","original_release_date")->where('id', '=', $id)->firstOrFail();
-
             $tmp = json_encode($game, JSON_FORCE_OBJECT);
-            $rs->getBody()->write($tmp);
             $rs = $rs->withHeader("Content-Type", "application/json");
+            $rs->getBody()->write($tmp);
         } catch (ModelNotFoundException $e){
             $rs = $rs->withHeader("Content-Type", "text/html");
             $rs->getBody()->write("<h1>Error 404</h1> Modele not found");
@@ -26,21 +26,23 @@ class Game
     }
 
     public function showAllGames(Request $rq, Response $rs, array $args): Response {
-            $page = $_GET["page"];
-            if (isset($_GET["page"])){
-                $games = \gamepedia\modele\Game::select("id","name","alias","deck")->skip(200*($_GET["page"]-1))->take(200)->get();
-            } else {
-                $games = \gamepedia\modele\Game::select("id","name","alias","deck")->take(200)->get();
-            }
+        $page = $_GET["page"];
 
-            $form["games"] = $games;
-            if (isset($_GET["page"])){
-               // $form["links"] = ["prev" => ["href" => "api/games/?="]]
-            }
-            $rs = $rs->withHeader("Content-Type", "application/json");
-            $tmp = json_encode($form, JSON_FORCE_OBJECT);
-            $rs->getBody()->write($tmp);
-            return $rs;
+        if (isset($page)){
+            $games = \gamepedia\modele\Game::select("id","name","alias","deck")->skip(200 * (page - 1))->take(200)->get();
+        } else {
+            $games = \gamepedia\modele\Game::select("id","name","alias","deck")->take(200)->get();
+        }
+
+        $form["games"] = $games;
+        if (isset($page)){
+            $form["links"] = ["prev" => ["href" => "api/games/?=" . strval($page - 1)], "next" => ["href" => "api/games/?=" . strval($page + 1)]];
+        }
+
+        $tmp = json_encode($form, JSON_FORCE_OBJECT);
+        $rs = $rs->withHeader("Content-Type", "application/json");
+        $rs->getBody()->write($tmp);
+        return $rs;
     }
 
 }
